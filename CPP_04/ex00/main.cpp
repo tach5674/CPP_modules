69331cb689ddea787f32faf5217785d068a5d8ab
Mikayel <tach5674@gmail.com>
2025-11-07 13:06:48 +0400

    ex00 done

---
+++ CPP_04/ex00/main.cpp
@@ -0,0 +1,33 @@
+#include "Dog.hpp"
+#include "Cat.hpp"
+#include "WrongCat.hpp"
+
+int main()
+{
+    std::cout << "------------------------------------" << std::endl;
+    {
+        const Animal *meta = new Animal();
+        const Animal *j = new Dog();
+        const Animal *i = new Cat();
+        std::cout << j->getType() << " " << std::endl;
+        std::cout << i->getType() << " " << std::endl;
+        i->makeSound(); // will output the cat sound!
+        j->makeSound();
+        meta->makeSound();
+        delete meta;
+        delete i;
+        delete j;
+    }
+    std::cout << "------------------------------------" << std::endl;
+    {
+        const WrongAnimal *meta = new WrongAnimal();
+        const WrongAnimal *i = new WrongCat();
+        std::cout << i->getType() << " " << std::endl;
+        i->makeSound(); // will output the cat sound!
+        meta->makeSound();
+        delete meta;
+        delete i;
+    }
+    std::cout << "------------------------------------" << std::endl;
+    return 0;
+}

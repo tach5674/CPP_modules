3babe9bc3074fb95415a3b518821ac880b785c03
Mikayel <tach5674@gmail.com>
2025-12-08 12:29:28 +0400

    ex01 done

---
+++ CPP_06/ex01/main.cpp
@@ -0,0 +1,19 @@
+#include "Serializer.hpp"
+#include <iostream>
+
+int main()
+{
+    Data *a = new Data;
+
+    a->i = 10;
+    std::cout << "Address of a: " << a << std::endl;
+    std::cout << "Value in a: " << a->i << std::endl;
+
+    Data *b = Serializer::deserialize(Serializer::serialize(a));
+
+    b->i = 5;
+    std::cout << "Address of b: " << b << std::endl;
+    std::cout << "Value in a: " << a->i << std::endl;
+
+    return 0;
+}

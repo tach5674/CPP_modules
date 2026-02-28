523858b8a4ad664955ff0792bea862d2fb41df9d
Mikayel Zohrabyan <mzohraby@c2r11s6.42yerevan.am>
2026-02-28 12:01:32 +0400

    fix

--- CPP_05/ex01/main.cpp
+++ CPP_05/ex01/main.cpp
@@ -3,22 +3,18 @@
 
 int main()
 {
-    Bureaucrat b = Bureaucrat("Robert", 10);
+    Bureaucrat b1 = Bureaucrat("Robert", 10);
+    Bureaucrat b2 = Bureaucrat("Tom", 30);
     Form f = Form("C8", 20, 5);
-    std::cout << b << std::endl;
+
+    std::cout << b1 << std::endl;
+    std::cout << b2 << std::endl;
     std::cout << f << std::endl;
-    // for (size_t i = 0; i < 150; i++)
-    // {
-    //     try
-    //     {
-    //         b.incGrade();
-    //     }
-    //     catch(std::exception &e)
-    //     {
-    //         std::cerr << e.what() << std::endl;
-    //     }
-    // }
-    b.signForm(f);
+
+    b2.signForm(f);
+    b1.signForm(f);
+
     std::cout << f << std::endl;
+    
     return 0;
 }
